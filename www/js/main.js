16807328850557af217d52df6d7aede8fe4c9696
Pablo44 <pawelrz@wp.pl>
2019-10-10 22:07:44 +0200

    creating body and child elements:main, header,footer,divs

---
+++ www/js/main.js
@@ -0,0 +1,15 @@
+let body = document.querySelector('body');
+let header = document.createElement('header');
+let main = document.createElement('main');
+let footer = document.createElement('footer');
+let div = document.createElement('div');
+let p = document.createElement('p');
+let a = documet.createElement('a');
+
+body.append(header,main,footer);
+header.append(div,p);
+main.append(div,p,a,input);
+footer.append(div,p,a);
+
+input.setAttribute('type','name','value');
+input.getAttribute('value');
