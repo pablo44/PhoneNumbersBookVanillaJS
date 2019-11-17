dd03af53afd0b5d440d1d31ffb4e06fb7cabe930
Pablo44 <pawelrz@wp.pl>
2019-11-18 00:32:54 +0100

    clas for the contact with id propert , additional functions

--- www/js/main.js
+++ www/js/main.js
@@ -4,45 +4,18 @@ let contacts = [];
 
 let body = document.querySelector('body');
 
-function refreshDOMWindow() {
+class Contact {
+    constructor(id, name, phone, email, history) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.history = [];
 
-    let contactContainer = document.createElement('div');
-    contactContainer.id = 'contactContainer';
-    contactContainer.className = 'contact-container';
+    }
+}
 
-    body.append(contactContainer);
-
-    let row = document.createElement('div');
-    let colName = document.createElement('div');
-    let colPhone = document.createElement('div');
-    let colEmail = document.createElement('div');
-    let colAddEntry = document.createElement('div');
-
-    row.className = 'contact-row';
-    colName.className = 'contact-column contact-name contact-header';
-    colPhone.className = 'contact-column contact-phone contact-header';
-    colEmail.className = 'contact-column contact-email contact-header';
-    colAddEntry.className = ' contact-column contact-add-entry contact-header';
-    colAddEntry.id = 'addNewEntry';
-
-    colName.innerHTML = 'Name';
-    colPhone.innerHTML = 'Phone';
-    colEmail.innerHTML = 'Email';
-    colAddEntry.innerHTML = '<i class="fas fa-user-plus"></i>';
-
-    contactContainer.append(row);
-    row.append(colName, colPhone, colEmail, colAddEntry);
-
-    let showContactBody = document.createElement('span');
-    showContactBody.id = 'showContactBody';
-    contactContainer.append(showContactBody);
-
-    let backdrop = document.createElement('div');
-    backdrop.className = 'disable-modal';
-    backdrop.id = 'backdrop';
-
-    body.append(backdrop);
-
+function createGetModalInputsData() {
     let newPersonModal = document.createElement('div');
     newPersonModal.className = 'disable-modal';
     newPersonModal.id = 'newPersonModal';
@@ -85,93 +58,179 @@ function refreshDOMWindow() {
     newPersonCancelBtn.value = 'Cancel';
     newPersonCancelBtn.id = 'newPersonCancelBtn';
 
+    let backdrop = document.createElement('div');
+    backdrop.className = 'disable-modal';
+    backdrop.id = 'backdrop';
+
+    body.append(backdrop);
+
     newPersonModal.append(lableModalName, inputName, lableModalPhone, inputPhone, lableModalEmail, inputEmail, newPersonSubmitBtn, newPersonCancelBtn);
 
-    let personHistoryModal = document.createElement('div');
-    personHistoryModal.className = 'disable-modal';
-    personHistoryModal.id = 'personHistoryModal';
+    newPersonSubmitBtn.addEventListener('click', () => {
+        let name = document.getElementById('newPersonName').value;
+        let phone = document.getElementById('newPersonPhone').value;
+        let email = document.getElementById('newPersonEmail').value;
+    })
 
-    body.append(personHistoryModal);
+    let id = contacts.length;
+    let contact = new Contact(id, name, phone, email, history);
+    contacts.push(contact);
+    let i = 0;
 
-    let header = document.createElement('h1');
-    header.innerHTML = 'History of the Contact' + contacts[i].name;
 
-    let currentContactData = document.createElement('ul');
-    currentContactData.className = 'list-data';
-    currentContactData.id = 'listHisotry';
+}
+function setCancelnewPersonModal(option) {
+    let newPersonName = document.getElementById('newPersonName');
+    let newPersonPhone = document.getElementById('newPersonPhone');
+    let newPersonEmail = document.getElementById('newPersonEmail');
+    newPersonName.value = '';
+    newPersonPhone.value = '';
+    newPersonEmail.value = '';
 
-    personHistoryModal.append(currentContactData);
+    let newPersonModal = document.getElementById('newPersonModal');
+    let backdrop = document.getElementById('backdrop');
 
-    let currentNameHist = document.createElement('li');
-    currentNameHist.innerHTML = contacts[i].name;
-
-    let currentPhoneHist = document.createElement('li');
-    currentPhoneHist.innerHTML = contacts[i].phone;
-
-    let currentEmailHist = document.createElement('li');
-    currentEmailHist.innerHTML = contacts[i].email;
-
-    currentContactData.append(currentNameHist, currentPhoneHist, currentEmailHist);
-
-    let historyRow = document.createElement('div');
-    historyRow.className = 'history-row';
-
-    personHistoryModal.append(historyRow);
-
-    let oldPhones = document.createElement('div');
-    oldPhones.className = 'history-col history-phones';
-    oldPhones.innerHTML = 'old phones';
+    newPersonModal.className = `${option}-modal`;
+    backdrop.className = `${option}-modal`;
+}
 
 
 
-    let oldPhonesList = document.createElement('ul');
-    oldPhonesList.className = 'old-phones';
 
-    let phoneItemHistoric = document.createElement('li');
-    phoneItemHistoric.id = 'phoneItmHistoric';
+let newEntrytBtn = document.getElementById('addNewEntry')
 
-    let phoneHolder = document.createElement('span');
-    phoneHolder.id = 'phoneHolder';
-    phoneHolder.innerHTML = setPhoneHistoric();
+newEntrytBtn =addEventListener('click', ()=>{
+    setCancelnewPersonModal('enable');
+})
 
-    let restoreBtnPhone = document.createElement('input');
-    restoreBtnPhone.type = 'submit';
-    restoreBtnPhone.value = 'Submit';
-    restoreBtnPhone.id = 'restoreMeBtnPhone';
-    restoreBtnPhone.innerHTML = 'Restore';
+// function refreshDOMWindow() {
 
-    let oldEmails = document.createElement('div');
-    oldEmails.className = 'history-col history-emails';
-    oldEmails.innerHTML = 'old phones';
+let contactContainer = document.createElement('div');
+contactContainer.id = 'contactContainer';
+contactContainer.className = 'contact-container';
 
-    let oldEmailsList = document.createElement('ul');
-    oldEmailsList.className = 'old-emails';
+body.append(contactContainer);
 
-    let emailItemHistoric = document.createElement('li');
-    emailItemHistoric.id = 'emailItmHistoric';
+let row = document.createElement('div');
+let colName = document.createElement('div');
+let colPhone = document.createElement('div');
+let colEmail = document.createElement('div');
+let colAddEntry = document.createElement('div');
 
-    let emailHolder = document.createElement('span');
-    emailHolder.id = 'emailHolder';
-    emailHolder.innerHTML = setEmailHistoric();
+row.className = 'contact-row';
+colName.className = 'contact-column contact-name contact-header';
+colPhone.className = 'contact-column contact-phone contact-header';
+colEmail.className = 'contact-column contact-email contact-header';
+colAddEntry.className = ' contact-column contact-add-entry contact-header';
+colAddEntry.id = 'addNewEntry';
 
-    let restoreBtnEmail = document.createElement('input');
-    restoreBtnEmail.type = 'submit';
-    restoreBtnEmail.value = 'Submit';
-    restoreBtnEmail.id = 'restoreMeBtnEmails';
-    restoreBtnEmail.innerHTML = 'Restore';
+colName.innerHTML = 'Name';
+colPhone.innerHTML = 'Phone';
+colEmail.innerHTML = 'Email';
+colAddEntry.innerHTML = '<i class="fas fa-user-plus"></i>';
 
-    historyRow.append(oldPhones, oldEmails);
 
-    oldPhones.append(oldPhonesList, phoneItemHistoric, phoneHolder, restoreBtnPhone);
-    oldEmails.append(oldEmailList, emailItemHistoric, emailHolder, restoreBtnEmail);
 
-    function setPhoneHistoric() {
-        let userhistory = contacts[i].history.phone;
-        for (let i = 0; i < userhistory.length; i++) {
-            let li = document.createElement('li');
-            li.innerHTML = + userhistory[i];
-        }
+contactContainer.append(row);
+row.append(colName, colPhone, colEmail, colAddEntry);
+
+let showContactBody = document.createElement('span');
+showContactBody.id = 'showContactBody';
+contactContainer.append(showContactBody);
+
+
+
+
+
+
+let personHistoryModal = document.createElement('div');
+personHistoryModal.className = 'disable-modal';
+personHistoryModal.id = 'personHistoryModal';
+
+body.append(personHistoryModal);
+
+let header = document.createElement('h1');
+
+
+// header.innerHTML = 'History of the Contact' + contacts[i].name;
+header.innerHTML = 'History of the Contact'
+
+let currentContactData = document.createElement('ul');
+currentContactData.className = 'list-data';
+currentContactData.id = 'listHisotry';
+
+personHistoryModal.append(currentContactData);
+
+let currentNameHist = document.createElement('li');
+currentNameHist.innerHTML = contacts[i].name;
+
+let currentPhoneHist = document.createElement('li');
+currentPhoneHist.innerHTML = contacts[i].phone;
+
+let currentEmailHist = document.createElement('li');
+currentEmailHist.innerHTML = contacts[i].email;
+
+currentContactData.append(currentNameHist, currentPhoneHist, currentEmailHist);
+
+let historyRow = document.createElement('div');
+historyRow.className = 'history-row';
+
+personHistoryModal.append(historyRow);
+
+let oldPhones = document.createElement('div');
+oldPhones.className = 'history-col history-phones';
+oldPhones.innerHTML = 'old phones';
+
+
+
+let oldPhonesList = document.createElement('ul');
+oldPhonesList.className = 'old-phones';
+
+let phoneItemHistoric = document.createElement('li');
+phoneItemHistoric.id = 'phoneItmHistoric';
+
+let phoneHolder = document.createElement('span');
+phoneHolder.id = 'phoneHolder';
+phoneHolder.innerHTML = setPhoneHistoric();
+
+let restoreBtnPhone = document.createElement('input');
+restoreBtnPhone.type = 'submit';
+restoreBtnPhone.value = 'Submit';
+restoreBtnPhone.id = 'restoreMeBtnPhone';
+restoreBtnPhone.innerHTML = 'Restore';
+
+let oldEmails = document.createElement('div');
+oldEmails.className = 'history-col history-emails';
+oldEmails.innerHTML = 'old phones';
+
+let oldEmailsList = document.createElement('ul');
+oldEmailsList.className = 'old-emails';
+
+let emailItemHistoric = document.createElement('li');
+emailItemHistoric.id = 'emailItmHistoric';
+
+let emailHolder = document.createElement('span');
+emailHolder.id = 'emailHolder';
+emailHolder.innerHTML = setEmailHistoric();
+
+let restoreBtnEmail = document.createElement('input');
+restoreBtnEmail.type = 'submit';
+restoreBtnEmail.value = 'Submit';
+restoreBtnEmail.id = 'restoreMeBtnEmails';
+restoreBtnEmail.innerHTML = 'Restore';
+
+historyRow.append(oldPhones, oldEmails);
+
+oldPhones.append(oldPhonesList, phoneItemHistoric, phoneHolder, restoreBtnPhone);
+oldEmails.append(oldEmailList, emailItemHistoric, emailHolder, restoreBtnEmail);
+
+function setPhoneHistoric() {
+    let userhistory = contacts[i].history.phone;
+    for (let i = 0; i < userhistory.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = + userhistory[i];
     }
+    // }
 
 
     //TO JEST DO EMAIL HOLDER,PHONE HOLDER
@@ -185,11 +244,12 @@ function refreshDOMWindow() {
     }
 
     //----------------------------------------START----------------------------
+    //refresh window and new contact holder - newContactBody
 
     contactContainer = document.getElementById('contactContainer');
     let prevContactContainer = document.getElementById('showContactBody');
     contactContainer.removeChild(prevContactContainer);
-
+    //here we are starting to create new elements
     let newContactBody = document.createElement('span');
     newContactBody.id = 'showContactBody';
 
@@ -200,10 +260,10 @@ function refreshDOMWindow() {
 
 
 
-} 
+}
 
-    
-    
+
+
 
 
 
